bba3ea8e2cacb05b3ba18f7bd4c853fc46432eaa
rdz <rdz.carlos@mail.com>
2019-06-03 20:36:04 -0500

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'multiacabados-orden';
+export class AppComponent implements OnInit {
+
+  public canvasWidth = 150;
+  public needleValue = 50;
+  public centralLabel = '';
+  public name = '';
+  public bottomLabel = '';
+  public options = {
+    hasNeedle: true,
+    needleColor: 'gray',
+    needleUpdateSpeed: 1000,
+    arcColors: ['red', 'yellow', 'green'],
+    arcDelimiters: [33, 67],
+    rangeLabel: ['E', 'F'],
+    needleStartValue: 60,
+  };
+  ngOnInit() {
+
+  }
+
+  combus(ev) {
+    // console.log(ev.srcElement.value);
+     this.needleValue = ev.srcElement.value;
+   }
 }
---
+++ src/assets/main.js
@@ -0,0 +1,68 @@
+var modal = document.getElementById('myModal');
+var modal2 = document.getElementById('myModal2');
+//var btn = document.getElementById("contSign");
+//var span = document.getElementsByClassName("close")[0];
+var span = document.getElementById('close');
+var canvas = document.getElementById('canvas');
+var span2 = document.getElementById('close2');
+var canvas2 = document.getElementById('canvas2');
+
+var signaturePad = new SignaturePad(canvas, {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    minWidth: 3,
+    maxWidth: 4,
+    penColor: "rgb(33, 33, 33)"
+});
+
+var signaturePad2 = new SignaturePad(canvas2, {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    minWidth: 3,
+    maxWidth: 4,
+    penColor: "rgb(33, 33, 33)"
+});
+//btn.onclick = function () {
+function btn_click() {
+    modal.style.display = "block";
+    resizeCanvas();
+}
+function btn_click2() {
+    modal2.style.display = "block";
+    resizeCanvas2();
+}
+span.onclick = function () {
+    modal.style.display = "none";
+    document.getElementById('imgSign').src = signaturePad.toDataURL();
+}
+span2.onclick = function () {
+    modal2.style.display = "none";
+    document.getElementById('imgSign2').src = signaturePad2.toDataURL();
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.getElementById('imgSign').src = signaturePad.toDataURL();
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+        document.getElementById('imgSign2').src = signaturePad2.toDataURL();
+    }
+}
+
+function resizeCanvas() {
+    var w = modal.clientWidth;
+    var h = modal.clientHeight;
+    canvas.width = Math.ceil(w * 0.75);
+    canvas.height = Math.ceil(h * 0.7);
+    signaturePad.clear();
+}
+function resizeCanvas2() {
+    var w = modal2.clientWidth;
+    var h = modal2.clientHeight;
+    canvas2.width = Math.ceil(w * 0.75);
+    canvas2.height = Math.ceil(h * 0.7);
+    signaturePad2.clear();
+}
+
+window.addEventListener("resize", resizeCanvas);
+window.addEventListener("resize", resizeCanvas2);
+
